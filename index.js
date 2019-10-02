4f5b94e8b2aa8186cf313b3ccce23b24aef30bc4
Alfian Azizi <alfianazizi4869@gmail.com>
2019-10-02 23:40:56 +0700

    added ref on device

--- index.js
+++ index.js
@@ -5,10 +5,11 @@ const express = require("express");
 const app = express();
 
 mongoose
-  .connect(
-    "mongodb+srv://spt-aquaculture:admin123qwe@democlusterazizi-wy1r4.mongodb.net/test?retryWrites=true&w=majority",
-    { useNewUrlParser: true }
-  )
+  // .connect(
+  //   "mongodb+srv://spt-aquaculture:admin123qwe@democlusterazizi-wy1r4.mongodb.net/test?retryWrites=true&w=majority",
+  //   { useNewUrlParser: true }
+  // )
+  .connect("mongodb://localhost:27017/stp", { useNewUrlParser: true })
   .then(() => console.log("Connected to MongoDB..."))
   .catch(err => console.error("Could not connect to MongoDB..."));
 
--- routes/sensors.js
+++ routes/sensors.js
@@ -5,7 +5,9 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/", async (req, res) => {
-  const sensors = await Sensor.find().sort("-timeStamp");
+  const sensors = await Sensor.find()
+    .populate("device", "deviceId")
+    .sort("-timeStamp");
   res.send(sensors);
 });
 
@@ -13,22 +15,18 @@ router.post("/", async (req, res) => {
   const { error } = validate(req.body);
   if (error) return res.status(400).send(error.details[0].message);
 
-  const device = await Device.findOne(req.body.deviceId);
-  console.log(device);
+  const device = await Device.findOne({ deviceId: req.body.deviceId });
   if (!device) return res.status(400).send("Invalid device.");
 
   let sensor = new Sensor({
     airTemp: req.body.airTemp,
+    waterTemp: req.body.waterTemp,
     ph: req.body.ph,
     salinity: req.body.salinity,
     uv: req.body.uv,
     windDirection: req.body.windDirection,
     windSpeed: req.body.windSpeed,
-    device: {
-      _id: device._id,
-      deviceId: device.deviceId,
-      deviceCreatedDate: device.createdDate
-    }
+    device: device._id
   });
   sensor = await sensor.save();
 
